cc4396ddfaa1bd1cd190b594ef17d799ed00901c
Mike Ball <mikedball@gmail.com>
2025-11-14 04:57:14 -0500

    fix: correct script installation

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Install homebrew if it's not already installed.
 # This should be compared to the latest installation
 # instructions at https://brew.sh before executing.
-if ! command -v brew &> /dev/null; then
+if ! command -v brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
@@ -20,7 +20,7 @@ mv "${HOME}/.gitconfig" "${HOME}/.gitconfig.bak" 2>/dev/null
 DOTFILES=${HOME}/dotfiles
 
 if [ ! -d "${HOME}/.config/nvim" ]; then
-  mkdir -p "${HOME}/.config/nvim";
+  mkdir -p "${HOME}/.config/nvim"
 fi
 
 ln -s "${DOTFILES}/rgignore" "${HOME}/.rgignore"
@@ -28,7 +28,7 @@ ln -s "${DOTFILES}/rgconfig" "${HOME}/.rgconfig"
 ln -s "${DOTFILES}/zshrc" "${HOME}/.zshrc"
 ln -s "${DOTFILES}/oh-my-zsh/themes/mdb.zsh-theme" "${HOME}/.oh-my-zsh/themes/mdb.zsh-theme"
 ln -s "${DOTFILES}/tmux.conf" "${HOME}/.tmux.conf"
-ln -s "${DOTFILES}/tmux.conf" "${HOME}/.tmux-osc52.sh"
+ln -s "${DOTFILES}/tmux-osc52.sh" "${HOME}/.tmux-osc52.sh"
 ln -s "${DOTFILES}/gitconfig" "${HOME}/.gitconfig"
 ln -s "${DOTFILES}/vimrc" "${HOME}/.vimrc"
 ln -s "${DOTFILES}/jshintrc" "${HOME}/.jshintrc"
@@ -48,4 +48,3 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 vim +PlugInstall +qall
 vim +GoInstallBinaries +qall
 vim +GoUpdateBinaries +qall
-vim :Copilot setup
